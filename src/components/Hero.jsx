5a30917e6d54018932b277968bab46e35d64d194
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-09 01:54:34 +0600

    nice one

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
-    const [loadedVideo, setLoadedVideo] = useState(0);
+    const [loadedVideos, setLoadedVideos] = useState(0);
 
-    const totalVideos = 4 ;
-    const nextVdRef = useRef(null);
+    const totalVideos = 3 ;
+    const nextVideoRef = useRef(null);
+
+    const handleVideoLoad = () =>{
+      setLoadedVideos((prev)=> prev + 1);
+    }
+
+    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
     const handleMiniVdClick = ()=> {
         setHasClicked(true);
 
-        setCurrentIndex((prevIndex)=> prevIndex + 1);
+        setCurrentIndex(upcomingVideoIndex);
     }
+
+    const getVideoSrc =(index) => `videos/hero-${index}.mp4`;
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
       <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div>
             <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
-                <div onClick={handleMiniVdClick}>
-                    Mini video Player
+                <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 hover:scale-100 transition-all duration-500 ease-in hover:opacity-100'>
+                    <video 
+                    ref={nextVideoRef}
+                    src={getVideoSrc(currentIndex + 1)}
+                    loop
+                    muted
+                    id='current-video'
+                    className='size-64 origin-center scale-150 object-cover object-center'
+                    onLoadedData={handleVideoLoad}
+                    />
                 </div>
             </div>
         </div>
