e4ce822635895a3b997547f34f4b84764437353b
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-11 00:26:59 +0600

    ok going on and on

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,19 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
-    const [loadedVideos, setLoadedVideos] = useState(0);
+    const [loadedVideos, setLoadedVideos] = useState(1);
 
-    const totalVideos = 3 ;
+    const totalVideos = 4 ;
     const nextVideoRef = useRef(null);
 
     const handleVideoLoad = () =>{
@@ -22,8 +25,15 @@ const Hero = () => {
     const handleMiniVdClick = ()=> {
         setHasClicked(true);
 
-        setCurrentIndex(upcomingVideoIndex);
+         setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
     }
+
+
+    useEffect(()=>{
+      if(loadedVideos === totalVideos - 1){
+        setIsLoading(false);
+      }
+    },[loadedVideos])
 
     useGSAP(()=> {
       if(hasClicked){
@@ -46,10 +56,36 @@ const Hero = () => {
       }
     }, {dependencies: [currentIndex],  revertOnUpdate: true})
 
+    useGSAP(()=>{
+      gsap.set('#video-frame',{clipPath: 'polygon(14% 0%, 72% 0%, 88% 90%, 0% 95%)',
+        borderRadius: '0% 0% 40% 10%'
+      })
+      gsap.from('#video-frame',{clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+        borderRadius: '0% 0% 0% 0%',
+        ease: 'power1.inOut',
+        scrollTrigger: {
+          trigger: '#video-frame',
+          start: 'center center',
+          end: 'bottom center',
+          scrub: true,
+        },
+      });
+    });
+
     const getVideoSrc =(index) => `videos/hero-${index}.mp4`;
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
+
+      {isLoading && (
+        <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
+          <div className='three-body '>
+            <div className='three-body__dot'></div>
+            <div className='three-body__dot'></div>
+            <div className='three-body__dot'></div>
+          </div>
+        </div>
+      )}
       <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div>
             <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,22 @@ export default {
         'circular-web': ['circular-web', 'sanf-serif'],
         'robert-medium' : ['robert-medium','sanf-serif'],
         'robert-regular' : ['robert-regular','sanf-serif'],
+      },
+      colors: {
+        blue: {
+          50: '#DFDFF0',
+          75: '#DFDFF2',
+          100: '#F0F2FA',
+          200: '#010101',
+          300: '#4FB7DD',
+        },
+        violet: {
+          300: '#5724FF'
+        },
+        yellow: {
+          100: '#8E983F',
+          300: '#EDFF66',
+        }
       }
     },
   },
