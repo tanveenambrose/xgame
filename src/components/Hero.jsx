baae9eaea09701dfbd2cbc086e86033682d1da15
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-10 11:18:14 +0600

    nice animation using gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -22,6 +24,27 @@ const Hero = () => {
 
         setCurrentIndex(upcomingVideoIndex);
     }
+
+    useGSAP(()=> {
+      if(hasClicked){
+        gsap.set('#next-video' ,{ visibility: 'visible' });
+        gsap.to( '#next-video', {
+          transformOrigin : 'center center',
+          scale: 1,
+          width: '100%' ,
+          height: '100%' ,
+          duration: 1,
+          ease: 'power1.inOut',
+          onStart: ()=> nextVideoRef.current.play(),
+        })
+        gsap.from('#current-video', {
+          transformOrigin: 'center center',
+          scale: 0,
+          duration: 1.5,
+          ease: 'power1.inOut',
+        })
+      }
+    }, {dependencies: [currentIndex],  revertOnUpdate: true})
 
     const getVideoSrc =(index) => `videos/hero-${index}.mp4`;
 
@@ -71,6 +94,9 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      <h1 className='special-font hero-heading absolute bottom-5 right-5  text-black'>
+          G<b>a</b>ming
+        </h1>
     </div>
   )
 }
