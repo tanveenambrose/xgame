59eb89a2d8a480ee3856f843672feaca7da00751
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-10 10:54:09 +0600

    ok doing it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useRef } from 'react'
+import Button from './Button';
+import { TiLocationArrow } from "react-icons/ti";
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -31,7 +33,7 @@ const Hero = () => {
                 <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 hover:scale-100 transition-all duration-500 ease-in hover:opacity-100'>
                     <video 
                     ref={nextVideoRef}
-                    src={getVideoSrc(currentIndex + 1)}
+                    src={getVideoSrc(upcomingVideoIndex)}
                     loop
                     muted
                     id='current-video'
@@ -40,6 +42,33 @@ const Hero = () => {
                     />
                 </div>
             </div>
+            <video 
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id='next-video'
+            className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+            onLoadedData={handleVideoLoad}/>
+            <video 
+            src={getVideoSrc(currentIndex === totalVideos -1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+            className='absolute left-0 top-0 size-full object-center object-cover'/>
+        </div>
+        <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-[#DFDFF2]'>
+          G<b>a</b>ming
+        </h1>
+        <div className='absolute left-0 top-0 z-40 size-full'>
+          <div className='mt-24 px-5 sm:px-10'>
+            <h1 className='special-font hero-heading text-blue-100'>redefi<b>n</b>e</h1>
+            <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+
+            <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow/>} containerClass="bg-yellow-300 flex-center gap-1"/>
+          </div>
         </div>
       </div>
     </div>
