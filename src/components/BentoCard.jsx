02539cb28d7ae2b5deba5884362001f53af3a312
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-11 17:36:22 +0600

    ok doing it good

---
+++ src/components/BentoCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const BentoCard = ({src, title,description, isComingSoon}) => {
+  return (
+    <div className='relative size-full'>
+        <video 
+        src={src}
+        loop
+        muted
+        autoPlay
+        className='absolute left-0 top-0 size-full object-cover object-center'
+        />
+        <div className='relative z-10 flex size-full flex-col justify-between p-5 text-blue-50'>
+            <div>
+                <h1 className='bento-title special-font '>{title}</h1>
+                {
+                    description && (
+                        <p className='mt-3 max-w-64 text-xs md:text-base'>{description}</p>
+                    )
+                }
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default BentoCard
