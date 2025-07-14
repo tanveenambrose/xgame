aa0b168d50ba7dabdf284820bd1ec80ac3175572
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-14 14:59:19 +0600

    ok done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
+    <div id='contact' className='my-20 min-h-96 w-screen px-10'>
+      <div className='relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden'>
+        <div className='absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96'>
+          <div className='contact-clip-path-1'></div>
+        </div>
+      </div>
     </div>
   )
 }
